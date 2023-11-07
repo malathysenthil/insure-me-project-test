25d156bdbbe38ac3c83617df3b6713ae6f8afb53
smalathy <mals.malathy@gmail.com>
2023-11-07 13:00:06 -0500

    Created Jar

--- src/test/java/test/ContactUsTest.java
+++ src/test/java/test/ContactUsTest.java
@@ -1,21 +1,22 @@
 package test;
-
-import org.testng.annotations.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ContactUsTest {
-	WebDriver driver;
-  @Test
-  public void testContactUs() throws InterruptedException {
-	  driver.get("http://107.22.93.101:8081/index.html");
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options= new ChromeOptions(); 
+		options.addArguments("--window-size=1920,1080");
+	    options.addArguments("--start-maximized");
+		options.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.get("http://107.22.93.101:8081/index.html");
 		driver.findElement(By.xpath("//*[text()='Contact Us']")).click();
 		driver.findElement(By.id("inputName")).sendKeys("Malathy");
 		driver.findElement(By.id("inputNumber")).sendKeys("1111111111");
@@ -24,24 +25,12 @@ public class ContactUsTest {
 		driver.findElement(By.xpath("//button[@id='my-button']")).click();
 		Thread.sleep(6000);
 		String msg = driver.findElement(By.id("response")).getText();
-		Assert.assertTrue(msg.equals("Message Sent"));  
-	  
-  }
-  @BeforeTest
-  public void beforeTest() {
-	  WebDriverManager.chromedriver().setup();
-		ChromeOptions options= new ChromeOptions(); 
-	//	options.setHeadless(true);
-		options.addArguments("--window-size=1920,1080");
-	    options.addArguments("--start-maximized");
-		options.addArguments("--headless");
-		driver = new ChromeDriver(options);
-  }
-
-  @AfterTest
-  public void afterTest() {
-	  
-	  driver.quit();
-  }
+		if (msg.equals("Message Sent")) {
+			System.out.println("Test case Passed");
+		} else {
+			System.out.println("Test case Failed");
+		}
+		driver.close();
+	}
 
 }
